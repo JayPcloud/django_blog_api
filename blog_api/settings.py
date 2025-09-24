593f770cdf10d7fdb0a6cbccedd234915d032440
JayPcloud <codwithjayp@gmail.com>
2025-09-24 18:09:41 +0100

    -

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-chmo2s2xxp^!^mk!_*64ltkvi-=732##n0s&oa^kr4uky!1e13'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-dev-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -143,12 +145,6 @@ CACHES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'codewithjayp@gmail.com'
-EMAIL_HOST_PASSWORD = 'Inquisitive12#'  # Use app-specific password
 
 
 # Internationalization
@@ -189,6 +185,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nwankwojohnpaul681@gmail.com'
-EMAIL_HOST_PASSWORD = 'vbivmpusdoafpche'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
